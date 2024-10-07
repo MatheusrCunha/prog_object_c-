829d3eee6355fe7b8b6a18d62f940adc5657ecd8
MatheusrCunha <matheus.rodriguesifsc@gmail.com>
2024-10-07 19:55:38 -0300

    olá mundo

---
+++ classe_teste/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+//#include <string>
+
+using namespace std;
+
+class World{
+
+private:
+    string _name;
+
+public:
+
+    World();
+    ~World();
+    void sayIt();
+    void setName(string value);
+
+};
+
+void World::setName(string value){
+
+    _name = value;
+
+}
+void World::sayIt(){
+
+    cout << "Hello " << _name << "!" <<  endl;
+}
+
+int main()
+{
+
+
+    World objeto;
+
+
+    objeto.setName("teste");
+    objeto.sayIt();
+    return 0;
+}
